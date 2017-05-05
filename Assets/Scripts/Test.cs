55e62f9ac1ba242e4db8306514cbd5176f11a0c9
Ian Holstead <ian.holstead@gmail.com>
2017-05-05 03:04:59 -0400

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using ControlWrapping;
+
+public class Test : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Logger.Log("test");
+        }
+    }
+}
