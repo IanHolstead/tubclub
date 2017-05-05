d0d864d25848a72aa4c287d28759d92d40053cab
Adam Leung <centribo@hotmail.com>
2017-05-05 14:23:18 -0400

    Upgrade to 5.6, added GameManager skeleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public static class GameManager {
-    //static Player[] playerList = new Player[4];
+[AddComponentMenu("Managers/GameManager")]
 
-    //public static int AddPlayer(Player player)
-    //{
-    //    for (int i = 0; i < playerList.Length; i++)
-    //    {
-    //        if (playerList[i] == null)
-    //        {
-    //            playerList[i] = player;
-    //            return i;
-    //        }
-    //    }
-    //    return -1;
-    //}
+public class GameManager : Singleton<GameManager> {
+
+	public enum State { Null };
+	public State state;
+
+	void Awake(){
+		DontDestroyOnLoad(transform.gameObject); //Don't destroy us on loading new scenes
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
-}
+	// Update is called once per frame
+	void Update () {
+		
+	}
 
-//public enum LogLevel
-//{
-//    None, Low, Medium, High, Verbose, Facist
-//}
+	public void LoadLevel(string level){
+		SceneManager.LoadScene(level);
+	}
+
+	public State GetState(){
+		return state;
+	}
+
+	public void SetState(State newState){
+		state = newState;
+	}
+}
