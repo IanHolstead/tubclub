efd70f93e57e795c536acfdfb3b1f8770c55f8cf
Adam Leung <centribo@hotmail.com>
2017-05-05 18:20:03 -0400

    Work on player controller and manager spawning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,32 +7,79 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager> {
 
-	public enum State { Null, Paused };
-	public State state;
+    [Header("Game Variables")]
+    public List<PlayerController> Players; //Player #1 is at index 0
 
-	void Awake(){
-		DontDestroyOnLoad(transform.gameObject); //Don't destroy us on loading new scenes
-	}
+    [Header("Prefab References")]
+    public GameObject PlayerPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum State { Null, Paused, Playing };
+    public State state;
 
-	public void LoadLevel(string level){
-		SceneManager.LoadScene(level);
-	}
+    void Awake(){
+        DontDestroyOnLoad(transform.gameObject); //Don't destroy us on loading new scenes
+    }
 
-	public State GetState(){
-		return state;
-	}
+    // Use this for initialization
+    void Start () {
+        SpawnPlayers(4);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
 
-	public void SetState(State newState){
-		state = newState;
-	}
+    public void SpawnPlayers(int num){ //Spawn the players, given number of players to spawn
+
+        if(num < 2 || num > 4) { //If we're trying to spawn an invalid # of player
+            Debug.LogError("Invalid number of players: " + num);
+            return;
+        }
+
+        Players = new List<PlayerController>(); //Clear the list
+
+        float degPerPlayer = 2 * Mathf.PI / num; //Calculate angle between each player spawn in radians
+        float spawnRadius = 5;
+
+        for(int i = 1; i < num+1; i++){ //For each we want to
+            Vector3 newPos = new Vector3(Mathf.Sin(degPerPlayer * i) * spawnRadius, 0, Mathf.Cos(degPerPlayer * i) * spawnRadius); //Calculate their spawn position
+            GameObject spawnedPlayer = Instantiate(PlayerPrefab, newPos, Quaternion.identity); //Spawn the player object
+            Players.Add(spawnedPlayer.GetComponent<PlayerController>()); //Add the newly spawned player to the players list
+            Players[i - 1].Setup(i); //Tell them to set themselves up
+        }
+
+        //Setup viewports
+        switch(num){ //Depending on the number of players,
+            case 2:
+                Players[0].camera.rect = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
+                Players[1].camera.rect = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
+            break;
+
+            case 3:
+                Players[0].camera.rect = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
+                Players[1].camera.rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
+                Players[2].camera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
+             break;
+
+            case 4:
+                Players[0].camera.rect = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
+                Players[1].camera.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+                Players[2].camera.rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
+                Players[3].camera.rect = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
+            break;
+        }
+    }
+
+    public void LoadLevel(string level){
+        SceneManager.LoadScene(level);
+    }
+
+    public State GetState(){
+        return state;
+    }
+
+    public void SetState(State newState){
+        state = newState;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,41 @@ using ControlWrapping;
 
 public class PlayerController : MonoBehaviour {
 
+    public AnimationCurve ac;
+
+    [Header("Player Variables")]
+    public int PlayerNum; //[1-4]
+
+    [Header("Component References")]
+    public Camera camera;
+
 	Gamepad gamepad;
 
 	// Use this for initialization
 	void Start () {
-		gamepad = ControllerManager.instance.RequestSpecificGamepad(0);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(gamepad.GetButtonDown(ActionKeyCode.GamepadA));
+		// Debug.Log(gamepad.GetButton(ActionKeyCode.GamepadA));
+        
+        UpdateSteering();
+        UpdateCamera();
 	}
+
+    public void Setup(int playerNum){ //Public call to setup our player
+        this.PlayerNum = playerNum; //Set our player number
+        this.name = "Player " + PlayerNum; //Set our name
+        gamepad = ControllerManager.instance.RequestSpecificGamepad(PlayerNum-1); //Get our gamepad reference
+    }
+
+    void UpdateSteering(){
+        // Debug.Log(gamepad.GetAxis(AxisCode.GamepadAxisLeftX));
+    }
+
+    void UpdateCamera(){
+        // Debug.Log(gamepad.GetAxis(AxisCode.GamepadAxisRightX));
+        // Debug.Log(gamepad.GetAxis(AxisCode.GamepadAxisRightY));
+    }
 }
