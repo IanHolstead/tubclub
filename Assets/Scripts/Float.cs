30e5cc3e3988825e5b7cde90a436435209167350
Ian Holstead <ian.holstead@gmail.com>
2017-05-05 23:31:31 -0400

    Added non-working floating script, prefab changes

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour {
+    public GameObject meshRef;
+    public float bobRate = .4f;
+    public float bobAmount = 2;
+
+    float age;
+	void Start () {
+        age = Random.Range(0f, 1000f);
+	}
+	
+	void Update () {
+        age += Time.deltaTime;
+
+        RaycastHit hit;
+
+        //TODO: this hits itself
+        if (Physics.Raycast(transform.position + Vector3.up*10, Vector3.up*-1, out hit, 100)){
+            Vector3 pos = transform.position;
+            pos.y = hit.point.y + BobUpAndDown();
+            transform.position = pos;
+        }
+	}
+
+    float BobUpAndDown()
+    {
+        return bobAmount * Mathf.Sin((age * bobRate) * Mathf.PI * 2);
+    }
+}
