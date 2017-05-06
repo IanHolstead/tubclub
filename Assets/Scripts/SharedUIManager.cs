9c1d647fb8d3f00c7952cf73208754bc9619507a
Adam Leung <centribo@hotmail.com>
2017-05-06 19:51:22 -0400

    Main game loop working, can start games repeatedly. Completed work on timer aswell

---
+++ Assets/Scripts/SharedUIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedUIManager : Singleton<SharedUIManager> {
+
+	[Header("Object References")]
+	public TimerController timerController;
+	public GameOverController gameOverMenu;
+
+	public void ShowTimer(){
+		timerController.gameObject.SetActive(true);
+	}
+
+	public void HideTimer(){
+		timerController.gameObject.SetActive(false);
+	}
+
+	public void UpdateTimer(float timer, float maxTime){
+		timerController.UpdateTimer(timer, maxTime);
+	}
+
+	public void ShowGameOverMenu(){
+		gameOverMenu.Show();
+	}
+}
