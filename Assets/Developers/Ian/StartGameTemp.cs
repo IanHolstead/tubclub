44363e336b57b9fb01d6116f72f9b37ded37c23d
Ian Holstead <ian.holstead@gmail.com>
2017-05-06 14:40:53 -0400

    WIP main scene putting everything together

---
+++ Assets/Developers/Ian/StartGameTemp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameTemp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameManager.Instance.state = GameManager.State.Playing;
+        GameManager.Instance.SpawnPlayers(GameManager.Instance.numberOfPlayers);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
