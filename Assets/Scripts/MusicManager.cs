6f4801aa2b532c4239ea147f805d04fe37db824f
Adam Leung <centribo@hotmail.com>
2017-05-06 23:03:34 -0400

    Added music looping and got working queueing of main music

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MusicManager : Singleton<MusicManager> {
 
 	public AudioSource[] IntroLoopSections;
+	public AudioSource MainGameMusic;
+	public bool MainGameMusicIsQueued;
+	public bool MainGameMusicIsPlaying;
 	public int LastLoopSection;
 	public int CurrentLoopSection;
 	public int NextLoopSection;
@@ -15,11 +18,20 @@ public class MusicManager : Singleton<MusicManager> {
 	// Use this for initialization
 	void Start () {
 		CurrentLoopSection = -1;
+		MainGameMusicIsQueued = false;
+		MainGameMusicIsPlaying = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		QueueNextSection();
+		if(Input.GetButton("Jump")){
+			MainGameMusicIsQueued = true;
+		}
+	}
+
+	public void PlayMainGameMusic(){
+		MainGameMusicIsQueued = true;
 	}
 
 	public void QueueNextSection(){
@@ -32,25 +44,35 @@ public class MusicManager : Singleton<MusicManager> {
 			CurrentLoopSection = 0;
 			NextLoopSection = 1;
 		} else {
-
 			if(IntroLoopSections[CurrentLoopSection].isPlaying){ //If the current section is playing, IE: We don't need to transition next,
-				//Queue up the next section
-				IntroLoopSections[NextLoopSection].PlayScheduled(initialAudioSettingsTime + audioTimer + IntroLoopSections[CurrentLoopSection].clip.length);
-			} else { //Otherwise, we need to change the sections for the next audio clip
-				LastLoopSection = CurrentLoopSection; //Set variables
-				CurrentLoopSection = NextLoopSection;
-				NextLoopSection ++;
-
-				if(NextLoopSection == IntroLoopSections.Length){ //If we're out of bounds,
-					LastLoopSection = IntroLoopSections.Length-2;
-					CurrentLoopSection = IntroLoopSections.Length-1;
-					NextLoopSection = 0;
+				if(!MainGameMusicIsPlaying){ //If the MainGameMusic isn't playing
+					//Queue up the next section
+					IntroLoopSections[NextLoopSection].PlayScheduled(initialAudioSettingsTime + audioTimer + IntroLoopSections[CurrentLoopSection].clip.length);
 				}
 
-				Debug.Log("C: " + CurrentLoopSection);
-				Debug.Log("N: " + NextLoopSection);
+				if(MainGameMusicIsQueued){ //If we're queued up to play the main game music next, 
+					MainGameMusic.PlayScheduled(initialAudioSettingsTime + audioTimer + IntroLoopSections[CurrentLoopSection].clip.length); //Queue it up as the next track,
+					IntroLoopSections[NextLoopSection].Stop(); //Don't queue up the track we just queued up
+					MainGameMusicIsQueued = false;
+					MainGameMusicIsPlaying = true;
+				}
+			} else { //Otherwise, we need to change the sections for the next audio clip
+				if(!MainGameMusicIsPlaying){
+					LastLoopSection = CurrentLoopSection; //Set variables
+					CurrentLoopSection = NextLoopSection;
+					NextLoopSection ++;
 
-				audioTimer += IntroLoopSections[NextLoopSection].clip.length;
+					if(NextLoopSection == IntroLoopSections.Length){ //If we're out of bounds,
+						LastLoopSection = IntroLoopSections.Length-2;
+						CurrentLoopSection = IntroLoopSections.Length-1;
+						NextLoopSection = 0;
+					}
+
+					Debug.Log("C: " + CurrentLoopSection);
+					Debug.Log("N: " + NextLoopSection);
+
+					audioTimer += IntroLoopSections[NextLoopSection].clip.length;
+				}
 			}
 		}
 	}
