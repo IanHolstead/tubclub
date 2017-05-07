6cbbea53cf769fd56eca12a1ba6bd667dbb7ee75
Ian Holstead <ian.holstead@gmail.com>
2017-05-07 00:29:43 -0400

    Added floatie movment script

---
+++ Assets/Scripts/Floaties.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floaties : MonoBehaviour {
+
+    [Header("Physics")]
+    public AnimationCurve RotationSpeedOverRadius; //Left, 0 | Right, 1 Sample this curve for rotation speed in degrees per second
+    public float radius; //Radius of our orbit, distance from 0, 0
+    public float radiusVelocity; //Positive => increasing radius
+                                 // Use this for initialization
+
+    public AnimationCurve RadiusReductionOverRadius;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePhysics();
+        ReduceRadius();
+	}
+
+    void UpdatePhysics()
+    {
+        radius = Mathf.Sqrt(Mathf.Pow(transform.position.x, 2) + Mathf.Pow(transform.position.z, 2)); //Get radius
+        radius += radiusVelocity * Time.deltaTime;
+        float angle = Mathf.Atan2(transform.position.x, transform.position.z) * Mathf.Rad2Deg; //Get our angle relative to 0,0
+        if (angle < 0)
+        { //If our angle is negative, we're under the x-axis
+            angle += 360; //Add 360 degrees
+        }
+        float deltaAngle = RotationSpeedOverRadius.Evaluate(radius); //Sample our curve to get our change in angle
+        angle -= deltaAngle;
+        angle = angle * Mathf.Deg2Rad; //Convert to radians
+        Vector3 newPos = new Vector3(Mathf.Sin(angle) * radius, transform.position.y, Mathf.Cos(angle) * radius); //Calculate their new position
+        transform.position = newPos; //Set the new position
+        transform.rotation = Quaternion.AngleAxis((angle * Mathf.Rad2Deg) - 90, Vector3.up);
+    }
+
+    void ReduceRadius()
+    {
+        radiusVelocity = RadiusReductionOverRadius.Evaluate(radius);
+    }
+}
