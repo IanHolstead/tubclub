2cdedccc885bd6b65d11cd8bbb2e1f601c666b2f
Adam Leung <centribo@hotmail.com>
2017-05-06 21:22:35 -0400

    Audio looping started, main game scene commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Managers/AudioManager")]
+
+[RequireComponent(typeof(AudioListener))]
+public class AudioManager : Singleton<AudioManager> {
+
+    AudioListener audioListener;
+
+    void Awake(){
+        DontDestroyOnLoad(transform.gameObject); //Don't destroy us on loading new scenes
+        audioListener = GetComponent<AudioListener>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : Singleton<MusicManager> {
+
+	public AudioSource[] IntroLoopSections;
+	public int LastLoopSection;
+	public int CurrentLoopSection;
+	public int NextLoopSection;
+	public double initialAudioSettingsTime;
+
+	double audioTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		CurrentLoopSection = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		QueueNextSection();
+	}
+
+	public void QueueNextSection(){
+		if(CurrentLoopSection == -1){ //If we're queueing for the first time,
+			initialAudioSettingsTime = AudioSettings.dspTime; //Set the initial first time
+			audioTimer = 0;
+			IntroLoopSections[0].Play(); //Play section 1
+
+			LastLoopSection = -1;
+			CurrentLoopSection = 0;
+			NextLoopSection = 1;
+		} else {
+
+			if(IntroLoopSections[CurrentLoopSection].isPlaying){ //If the current section is playing, IE: We don't need to transition next,
+				//Queue up the next section
+				IntroLoopSections[NextLoopSection].PlayScheduled(initialAudioSettingsTime + audioTimer + IntroLoopSections[CurrentLoopSection].clip.length);
+			} else { //Otherwise, we need to change the sections for the next audio clip
+				LastLoopSection = CurrentLoopSection; //Set variables
+				CurrentLoopSection = NextLoopSection;
+				NextLoopSection ++;
+
+				if(NextLoopSection == IntroLoopSections.Length){ //If we're out of bounds,
+					LastLoopSection = IntroLoopSections.Length-2;
+					CurrentLoopSection = IntroLoopSections.Length-1;
+					NextLoopSection = 0;
+				}
+
+				Debug.Log("C: " + CurrentLoopSection);
+				Debug.Log("N: " + NextLoopSection);
+
+				audioTimer += IntroLoopSections[NextLoopSection].clip.length;
+			}
+		}
+	}
+}
