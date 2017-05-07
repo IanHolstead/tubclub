0d500e9374994853fda692fab7d93b8cdec5e438
Adam Leung <centribo@hotmail.com>
2017-05-07 15:00:49 -0400

    Work on menu

---
+++ Assets/Scripts/SimpleFloat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFloat : MonoBehaviour {
+
+	public float bobRate;
+	public float bobAmount;
+
+
+	Vector3 origin;
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		time = Random.Range(0f, 1000f);
+		origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		float height = bobAmount/2 * Mathf.Sin((time * bobRate) * Mathf.PI * 2) - bobAmount/2;
+		height *= -1;
+		Debug.Log(height);
+
+		transform.position = new Vector3(origin.x, origin.y + height, origin.z);
+	}
+}
